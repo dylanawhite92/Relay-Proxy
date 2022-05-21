00cf497ee0ff31834ceab2fa1c88afc4b0cc8932
The Crystal King <dylanawhite11@gmail.com>
2022-05-21 11:29:31 -0500

    fetchWeather function added

--- public/main.js
+++ public/main.js
@@ -2,3 +2,21 @@
 const weatherDisplay = document.querySelector(".weather");
 const weatherForm = document.querySelector("#weather-form");
 const cityInput = document.querySelector("#city-input");
+
+// Fetch weather from API
+const fetchWeather = async (city) => {
+    const url = `/api?q=${city}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    if (data.cod === "404") {
+        alert("City not found.");
+        return;
+    }
+
+    if (data.cod === "401") {
+        alert("Invalid API Key.");
+        return;
+    }
+}
