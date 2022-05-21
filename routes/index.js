1a66b10361d3e3b63230f6aa80c9d51a16a96a06
The Crystal King <dylanawhite11@gmail.com>
2022-05-21 13:59:59 -0500

    adding env variables

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const needle = require("needle");
 
-router.get("/", (req, res) => {
-    res.json({ success: true });
-})
+// Env variables
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
 module.exports = router;
