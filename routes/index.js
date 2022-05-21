3dfbdaca871d96d9e463c6f139710f33b63a02ca
The Crystal King <dylanawhite11@gmail.com>
2022-05-21 14:02:29 -0500

    promise with try catch error handler, receiving 401 error as expected

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,15 @@ const API_BASE_URL = process.env.API_BASE_URL;
 const API_KEY_NAME = process.env.API_KEY_NAME;
 const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
+router.get("/", async (req, res) => {
+    try {
+        const apiRes = await needle("get", `${API_BASE_URL}`);
+        const data = apiRes.body;
+
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({error});
+    }
+})
+
 module.exports = router;
