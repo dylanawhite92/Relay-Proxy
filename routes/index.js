0e36a86393f04f856f13ab49d0c93101ca9cf948
The Crystal King <dylanawhite11@gmail.com>
2022-05-21 14:10:46 -0500

    added url search params, able to query by city in postman by adding ?q=CITY to route

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const url = require("url");
 const express = require("express");
 const router = express.Router();
 const needle = require("needle");
@@ -9,7 +10,13 @@ const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
 router.get("/", async (req, res) => {
     try {
-        const apiRes = await needle("get", `${API_BASE_URL}`);
+        // url search params, add query onto url
+        const params = new URLSearchParams({
+            [API_KEY_NAME]: API_KEY_VALUE,
+            ...url.parse(req.url, true).query
+        })
+
+        const apiRes = await needle("get", `${API_BASE_URL}?${params}`);
         const data = apiRes.body;
 
         res.status(200).json(data);
