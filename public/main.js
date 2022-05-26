cf7a810556456720435b458926f5319d6c9b24d8
The Crystal King <dylanawhite11@gmail.com>
2022-05-26 13:01:35 -0500

    added favico, modified displayData obj

--- public/main.js
+++ public/main.js
@@ -25,18 +25,11 @@ const fetchWeather = async (city) => {
     // replace several function calls later
     const displayData = {
         city: data.name,
-        metric: {
-            celsius: kelvinToCelsius(data.main.temp),
-            high_celsius: kelvinToCelsius(data.main.temp_max),
-            low_celsius: kelvinToCelsius(data.main.temp_min),
-            feels_like: kelvinToCelsius(data.main.feels_like),
-        },
-        imperial: {
-            farenheit: kelvinToFahrenheit(data.main.temp),
-            high_farenheit: kelvinToFahrenheit(data.main.temp_max),
-            low_farenheit: kelvinToFahrenheit(data.main.temp_min),
-            feels_like: kelvinToFahrenheit(data.main.feels_like),
-        },
+        celsius: kelvinToCelsius(data.main.temp),
+        farenheit: kelvinToFahrenheit(data.main.temp),
+        high_farenheit: kelvinToFahrenheit(data.main.temp_max),
+        low_farenheit: kelvinToFahrenheit(data.main.temp_min),
+        feels_like: kelvinToFahrenheit(data.main.feels_like),
         precipitation: checkForPrecipitation(data.precipitation), // 1hour, API field is undefined if no rain
         description: data.weather[0].description,
         sunrise: data.sys.sunrise,
@@ -56,7 +49,7 @@ const fetchWeather = async (city) => {
 const addWeatherToDOM = data => {
     weatherDisplay.innerHTML = `
         <h1> Weather in ${data.city}, ${data.country}</h1>
-        <h2>${data.imperial.farenheit} &deg;F | ${data.metric.celsius} &deg;C</h2>
+        <h2>${data.farenheit} &deg;F | ${data.celsius} &deg;C</h2>
     `
     
     cityInput.value = "";
@@ -70,8 +63,6 @@ const addWeatherToDOM = data => {
 // }
 
 // API's default value is Kelvin, so we convert to Farenheit/Celsius
-// come back and try to refactor these so we aren't making several function calls
-// just make one and pass all data back to displayData obj
 const kelvinToFahrenheit = temp => {
     return Math.ceil(((temp - 273.15) * 9) / 5 + 32);
 }
